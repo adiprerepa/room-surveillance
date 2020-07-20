11349797a5ba6043d1600ab97e53a70518db2932
Aditya Prerepa <adiprerepa@gmail.com>
2020-07-19 17:12:06 -0700

    cmake works

--- raspberry-cpp-server/src/main.cc
+++ raspberry-cpp-server/src/main.cc
@@ -1,10 +1,10 @@
 #include <iostream>
-// #include "proto/activity_interface.pb.h"
+#include "proto/activity_interface.pb.h"
 #include "utils.h"
 
 int main() {
-  // GOOGLE_PROTOBUF_VERIFY_VERSION;
-  // Esp8266Alert::AlertThresholdCross alert = Esp8266Alert::AlertThresholdCross();
-  // alert.set_crossed_thresh(20);
-  std::cout << "yo" << std::endl;
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+  Esp8266Alert::AlertThresholdCross alert = Esp8266Alert::AlertThresholdCross();
+  alert.set_crossed_thresh(20);
+  std::cout << alert.crossed_thresh() << std::endl;
 }
